86869473da0386b7eaf4f57489735488e96e92ef
msimonz <simon.marquezok@gmail.com>
2025-02-22 18:01:37 -0500

    Arreglo para todos

--- Functions/header/header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuBtn = document.querySelector(".menu-hamburguesa");
-    const navMenu = document.querySelector(".items-navegacion");
-
-    menuBtn.addEventListener("click", function () {
-        navMenu.classList.toggle("active");
-    });
-});
