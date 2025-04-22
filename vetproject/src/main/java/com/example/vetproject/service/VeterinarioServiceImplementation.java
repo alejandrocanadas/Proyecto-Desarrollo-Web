6a37c700674a5cb5302ea25494ace684c52e52c9
Alejandro Canadas <alejandrocanadas113@gmail.com>
2025-04-22 15:52:33 -0500

    Correccion veterinario 2

--- vetproject/src/main/java/com/example/vetproject/service/VeterinarioServiceImplementation.java
+++ vetproject/src/main/java/com/example/vetproject/service/VeterinarioServiceImplementation.java
@@ -3,6 +3,7 @@ package com.example.vetproject.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.vetproject.entity.Cliente;
 import com.example.vetproject.entity.Veterinario;
