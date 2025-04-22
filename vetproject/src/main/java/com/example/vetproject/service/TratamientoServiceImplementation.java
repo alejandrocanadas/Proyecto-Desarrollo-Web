ad1a07a14d32eaaff1101cd78aab3de412a47726
msimonz <simon.marquezok@gmail.com>
2025-04-22 14:30:56 -0500

    A Alejo le gustan los hombres

--- vetproject/src/main/java/com/example/vetproject/service/TratamientoServiceImplementation.java
+++ vetproject/src/main/java/com/example/vetproject/service/TratamientoServiceImplementation.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.vetproject.repository.TratamientoRepository;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TratamientoServiceImplementation implements TratamientoService {
 
     @Autowired
